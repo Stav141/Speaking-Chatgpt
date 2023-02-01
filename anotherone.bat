57b747c534dcb53aaedf79eea57af6367cc56d96
Stav141 <124206308+Stav141@users.noreply.github.com>
2023-02-01 21:52:10 +0100

    Update anotherone.bat

--- anotherone.bat
+++ anotherone.bat
@@ -1,5 +1,5 @@
 cd "C:\Users\User\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\LocalCache\local-packages\Python310\site-packages\chatgpt_wrapper"
-python chatgpt.py install <---- lead the path to Chatgpt_wrapper (don't remove "chatgpt.py install" from this line")
+python chatgpt.py install #<---- lead the path to Chatgpt_wrapper (don't remove "chatgpt.py install" from this line")
 
-cd "D:\New folder" <--- Lead this dirrectory to this current folder
+cd "D:\New folder" #<--- Lead this dirrectory to this current folder
 start Voiced-GPT.bat
